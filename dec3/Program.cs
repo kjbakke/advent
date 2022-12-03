fe90dc83836e41c7e953637fe8c9e3d817ee9c2f
Karl-Johan Bakke <karljohan.bakke@gmail.com>
2022-12-03 13:37:24 +0100

    dec3

---
+++ dec3/Program.cs
@@ -0,0 +1,31 @@
+﻿var backpacks = File.ReadAllText("pattern3").Split(
+    new string[] { Environment.NewLine },
+    StringSplitOptions.RemoveEmptyEntries);
+var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var prio = new Dictionary<char, int>();
+int value = 1;
+
+foreach (var letter in letters)
+{
+    prio.Add(Char.ToLower(letter), value);
+    prio.Add(letter, value + 26);
+    value++;
+}
+
+value = 0;
+var group = new List<string>();
+
+foreach (var backpack in backpacks)
+{
+    group.Add(backpack);
+    if (group.Count == 3)
+    {
+        value += prio[group[0].FirstOrDefault(_ => group[1].Contains(_) && group[2].Contains(_))];
+        group.Clear();
+    }
+}
+
+Console.Write(value);
+
+
