f6403f86ea0892ac349a1b9f152801624e58f6bc
Karl-Johan Bakke <karljohan.bakke@gmail.com>
2022-12-05 14:49:41 +0100

    dec4

---
+++ dec4/Program.cs
@@ -0,0 +1,23 @@
+﻿var pattern = File.ReadAllText("pattern5").Replace("move ", "").Replace("from ", "").Replace("to ", "").Split(
+    new string[] { Environment.NewLine },
+    StringSplitOptions.RemoveEmptyEntries);
+
+var stacks = new Dictionary<int, List<char>>()
+{
+    { 1, new List<char>() { 'N', 'W', 'F', 'R', 'Z', 'S', 'M', 'D' } }
+};
+var counter = 0;
+foreach (var part in pattern)
+{
+    var pair = part.Split(new string[] { "-", "," }, StringSplitOptions.None);
+    int[] id = new int[4] { Int32.Parse(pair[0]), Int32.Parse(pair[1]), Int32.Parse(pair[2]), Int32.Parse(pair[3])};
+    
+    if (id[0] <= id[2] && id[1] >= id[2])
+        counter++;
+    else if (id[2] <= id[0] && id[3] >= id[0])
+        counter++;
+}
+
+
+Console.WriteLine(counter);
+
