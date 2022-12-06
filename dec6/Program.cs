d0d98893eed114f031db218b86cbeaff32cda162
Karl-Johan Bakke <karljohan.bakke@gmail.com>
2022-12-06 23:04:08 +0100

    dec6

---
+++ dec6/Program.cs
@@ -0,0 +1,28 @@
+﻿var pattern = File.ReadAllText("pattern6");
+//pattern = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+var code = "";
+var count = 0;
+
+foreach (var bit in pattern)
+{
+    foreach (var part in code)
+    {
+        if (part.Equals(bit))
+        {
+            if (code.Length > 1)
+                code = code.Substring(code.IndexOf(part) + 1, code.Length - (code.IndexOf(part) + 1));
+            else
+                code = "";
+            break;
+        }
+    }
+    
+    code += bit;
+    count++;
+    if (code.Length == 14)
+    {
+        Console.WriteLine(count);
+        break;
+    }
+
+}
